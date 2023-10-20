d078c9f2cf3a6e21d65e55086717264b3d81e02a
shailendra kumar <shailendraftp000@gmail.com>
2023-10-21 00:38:03 +0530

    some changes in env related issue

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:3000",
-  //       secure: false,
-  //     },
-  //   },
-  // },
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
-});
+})
