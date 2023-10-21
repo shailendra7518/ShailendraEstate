c2087b5281032090622a1a4bbc99c08761aa62e3
shailendra kumar <shailendraftp000@gmail.com>
2023-10-21 15:00:44 +0530

    added redux toolkit

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 const apiUrl = import.meta.env.VITE_BASE_URL;
 import { Link, useNavigate } from "react-router-dom";
+import {signInStart,signInSuccess,signInFailure} from '../Redux/User/userSlice'
+
 function SignIn() {
   const [formData, setformData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+   const {loading, error}=useSelector((state)=>state.user)
+  const dispatch = useDispatch();
   const Navigate = useNavigate();
   const handleChange = (e) => {
     setformData({ ...formData, [e.target.id]: e.target.value });
   };
-console.log(formData)
+  console.log(formData)
+  
+  
   const handleSubmit = async (e) => {
+    // prevent default feature
     e.preventDefault();
     try {
-      setLoading(true);
-
+      // setLoading(true);
+      // dispatch signin start function
+      dispatch(signInStart());
+     // call the api endpoint
       const res = await fetch(`${apiUrl}/api/auth/signin`, {
         method: "POST",
         headers: {
@@ -22,19 +30,21 @@ console.log(formData)
         },
         body: JSON.stringify(formData),
       });
-      const data = await res.json();
        
-      setError(data);
-      setLoading(false);
+      const data = await res.json();
+       console.log(data)
+   
+      // check if status code 201 then its successfully loged in
       if (data.status == 201) {
-      
-        Navigate("/");
+         dispatch(signInSuccess(data))
+        // Navigate("/");
+      } else {
+        dispatch(signInFailure(data))
       }
 
-      console.log(data);
+      // console.log(data);
     } catch (err) {
-      setLoading(false);
-      setError(err.message);
+      dispatch(signInFailure(err))
     }
   };
 
