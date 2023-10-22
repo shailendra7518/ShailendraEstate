9b9facfec1d258de43683d1ff4b275547fb9efa0
shailendra kumar <shailendraftp000@gmail.com>
2023-10-22 15:43:59 +0530

    added delete-user functionality

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,76 +1,118 @@
-import React, { useDebugValue, useEffect, useRef, useState } from 'react'
+import React, { useDebugValue, useEffect, useRef, useState } from "react";
 const apiUrl = import.meta.env.VITE_BASE_URL;
-import { useDispatch, useSelector } from 'react-redux'
-import { getStorage, uploadBytesResumable ,ref, getDownloadURL} from 'firebase/storage'
-import { app } from '../firebase'
-import { updateUserStart ,updateUserFailure,updateUserSuccess} from '../Redux/User/userSlice'
- 
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getStorage,
+  uploadBytesResumable,
+  ref,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../firebase";
+import {
+  updateUserStart,
+  updateUserFailure,
+  updateUserSuccess,
+  deleteUserStart,
+  deleteUserFailure,
+  deleteUserSuccess,
+} from "../Redux/User/userSlice";
+
 function Profile() {
   const dispatch = useDispatch();
-  const { currentUser ,loading,error} = useSelector((state) => state.user)
-  const fileRef = useRef(null)
-  const [file, setFile] = useState(undefined)
-  const [filePerc, setFilePerc] = useState(0)
-  const [fileUploadError, setFileUploadError] = useState(false)
-  const [formData, setformData] = useState({})
-  const [updateSuccess,setUpdateSuccess]=useState(false)
-  
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setformData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  // handleChange function to track the changes in form
   const handleChange = (e) => {
-    setformData({...formData,[e.target.id]:e.target.value})
-  }
-// make the  request to backend to update profile
+    setformData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  // make the  request to backend to update profile
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       dispatch(updateUserStart());
-      
+
       const res = await fetch(
         `${apiUrl}/api/users/update/${currentUser.user._id}`,
         {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
-           "token": `${currentUser.token}`,
+            token: `${currentUser.token}`,
           },
           body: JSON.stringify({ ...formData, id: currentUser.user._id }),
         }
       );
-      
+
       // convert to json;
       const data = await res.json();
       if (data.status == 200) {
-        setUpdateSuccess(true)
-        dispatch(updateUserSuccess(data))
+        setUpdateSuccess(true);
+        dispatch(updateUserSuccess(data));
       } else {
-        dispatch(updateUserFailure(data))
+        dispatch(updateUserFailure(data));
       }
-
-  
-      
     } catch (err) {
       dispatch(updateUserFailure(err));
     }
-  }
-  // console.log(file)
-  console.log("formdata",formData)
+  };
+
+  // handleDelete function to delete the user;
+  const handleDelete = async () => {
+    try {
+      dispatch(deleteUserStart());
+      
+      const res = await fetch(
+        `${apiUrl}/api/users/delete/${currentUser.user._id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            token: `${currentUser.token}`,
+          },
+          body: JSON.stringify({ id: currentUser.user._id }),
+        }
+      );
+      
+      const data = await res.json();
+      console.log(data)
+      if (data.status == 201) {
+         dispatch(deleteUserSuccess(data))
+      } else {
+        dispatch(deleteUserFailure(data))
+      }
   
+    } catch (err) {
+      dispatch(deleteUserFailure(err))
+}
+
+  };
+
+  // console.log(file)
+  console.log("formdata", formData);
+
   useEffect(() => {
-    if (file ) {
-      handleFileUpload(file)
+    if (file) {
+      handleFileUpload(file);
     }
-  }, [file])
-   console.log(`upload prgress ${filePerc}% done`);
+  }, [file]);
+  console.log(`upload prgress ${filePerc}% done`);
   const handleFileUpload = (file) => {
     const storage = getStorage(app);
-    const fileName = new Date().getTime()+file.name;
+    const fileName = new Date().getTime() + file.name;
     const storageRef = ref(storage, fileName);
-    const uploadTask = uploadBytesResumable(storageRef, file)
+    const uploadTask = uploadBytesResumable(storageRef, file);
     uploadTask.on(
       "state_changed",
       (snapshot) => {
-        const progress =(snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         setFilePerc(Math.round(progress));
-       
       },
       (error) => {
         setFileUploadError(true);
@@ -81,9 +123,7 @@ function Profile() {
         );
       }
     );
-   
-    
-  }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -139,18 +179,23 @@ function Profile() {
         />
         <button
           disabled={loading}
-          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
-          {loading ?"Loading...":"Update"}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span onClick={handleDelete} className="text-red-700 cursor-pointer">
+          Delete Account
+        </span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
-      <p className='text-green-700'>{updateSuccess? "updated successfully" :""}</p>
-      <p className='text-red-700'>{error ? error.message :""}</p>
+      <p className="text-green-700">
+        {updateSuccess ? "updated successfully" : ""}
+      </p>
+      <p className="text-red-700">{error ? error.message : ""}</p>
     </div>
   );
 }
 
-export default Profile
+export default Profile;
