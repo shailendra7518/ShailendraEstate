7706f1da24bc60ffd9034650a62737d2335c0d7e
shailendra kumar <shailendraftp000@gmail.com>
2023-10-23 22:23:56 +0530

    show-user-listing

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -28,6 +28,8 @@ function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setformData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingError, setShowListingError] = useState(false);
+  const [userListings,setUserListings]=useState([])
   // handleChange function to track the changes in form
   const handleChange = (e) => {
     setformData({ ...formData, [e.target.id]: e.target.value });
@@ -68,7 +70,7 @@ function Profile() {
   const handleDelete = async () => {
     try {
       dispatch(deleteUserStart());
-      
+
       const res = await fetch(
         `${apiUrl}/api/users/delete/${currentUser.user._id}`,
         {
@@ -80,25 +82,23 @@ function Profile() {
           body: JSON.stringify({ id: currentUser.user._id }),
         }
       );
-      
-      const data = await res.json();
-      console.log(data)
-      if (data.status == 201) {
-         dispatch(deleteUserSuccess(data))
-      } else {
-        dispatch(deleteUserFailure(data))
-      }
-  
-    } catch (err) {
-      dispatch(deleteUserFailure(err))
-}
 
+      const data = await res.json();
+      console.log(data);
+      if (data.status == 201) {
+        dispatch(deleteUserSuccess(data));
+      } else {
+        dispatch(deleteUserFailure(data));
+      }
+    } catch (err) {
+      dispatch(deleteUserFailure(err));
+    }
   };
 
   // handleLogout to logout user
   const handleLogout = () => {
-      dispatch(logoutUserSuccess("user delete successfull"))
-  }
+    dispatch(logoutUserSuccess("user delete successfull"));
+  };
 
   // console.log(file)
   console.log("formdata", formData);
@@ -131,6 +131,36 @@ function Profile() {
       }
     );
   };
+
+  const handleShowListing = async () => {
+    try {
+      console.log("hello")
+      setShowListingError(false);
+      const res = await fetch(
+        `${apiUrl}/api/users/listings/${currentUser.user._id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "token": currentUser.token,
+            'id':currentUser.user._id
+          },
+          
+        }
+      );
+      console.log("data", res);
+      const data = await res.json();
+      console.log("data",data);
+      if (data.status == 200) {
+        setUserListings(data.listings)
+        setShowListingError(false);
+      
+      }
+    } catch (err) {
+      setShowListingError(true);
+    }
+  };
+console.log(userListings)
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -190,20 +220,61 @@ function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
-        <Link className="bg-green-700 text-white rounded-lg p-3 uppercase text-center hover:opacity-95" to={"/create-listing"}>create listing</Link>
+        <Link
+          className="bg-green-700 text-white rounded-lg p-3 uppercase text-center hover:opacity-95"
+          to={"/create-listing"}
+        >
+          create listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDelete} className="text-red-700 cursor-pointer">
           Delete Account
         </span>
-        <span
-          onClick={handleLogout}
-          className="text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleLogout} className="text-red-700 cursor-pointer">
+          Sign out
+        </span>
       </div>
       <p className="text-green-700">
         {updateSuccess ? "updated successfully" : ""}
       </p>
       <p className="text-red-700">{error ? error.message : ""}</p>
+
+      <button onClick={handleShowListing} className="text-green-700 w-full">
+        Show Listing
+      </button>
+      <p className="text-red-700 mt-5">
+        {showListingError ? "Error showing listing" : ""}
+      </p>
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">Your Listing</h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className=" border rounded-lg p-3 flex justify-between items-center gap-4 "
+            >
+              <Link
+                className="text-slate-700font-semibold flex-1 hover:underline truncate"
+                to={`/listing/${listing._id}`}
+              >
+                <img
+                  className="h-16 w-16 object-contain "
+                  src={listing.imageUrls[0]}
+                  alt="listing image"
+                />
+              </Link>
+              <Link to={`/listing/${listing._id}`}>
+                <p>{listing.name}</p>
+              </Link>
+              <div className=" flex flex-col item-center ">
+                <button className="text-red-700 uppercase">Delete</button>
+                <button className="text-green-700 uppercase">Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
