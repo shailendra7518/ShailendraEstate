736d49427fa689cb84b451cc08d8a349f52642fd
shailendra kumar <shailendraftp000@gmail.com>
2023-10-26 02:23:15 +0530

    completed about page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import CreateListing from "./Pages/CreateListing";
 import UpdateListing from "./Pages/UpdateListing";
 import Listing from "./Pages/Listing";
 import Search from "./Pages/Search";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           <Route path="/update-listing/:id" element={<UpdateListing />} />
         </Route>
       </Routes>
+      <Footer/>
     </>
   );
 }
